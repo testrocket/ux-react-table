fe0c008f44858f496442bc12c505d9fba807b4d1
konrad.kujawa <konrad.kujawa@pragmatists.pl>
2019-04-08 20:57:14 +0200

    added export to CSV button with download impl

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Table from './components/table';
 import sampleModel from './models/SampleModel';
+import { exportToCSV, downloadContent } from './services/exporter';
 
 class App extends Component {
 
@@ -13,10 +14,15 @@ class App extends Component {
     };
   }
 
+  exportAsCSVFile = () => {
+    downloadContent(exportToCSV(this.state.model));
+  }
+
   render() {
     return (
       <div className="App">
         <Table model={this.state.model}/>
+        <button className="export-button" onClick={this.exportAsCSVFile}>Export to CSV</button>
       </div>
     );
   }
--- src/services/exporter/index.js
+++ src/services/exporter/index.js
@@ -7,3 +7,17 @@ export function exportToCSV(model) {
     .join('\n')
     .value();
 }
+
+export function downloadContent(content) {
+  const csvFileName = `download-${new Date().getTime()}.csv`;
+  if (navigator.msSaveBlob) {
+    const data = new Blob([content], { type: 'application/bpmn20-xml;charset=UTF-8' });
+    navigator.msSaveBlob(data, csvFileName);
+  } else {
+    const link = document.createElement('a');
+    link.href = `data:application/bpmn20-xml;charset=UTF-8,${content}`;
+    link.target = '_target';
+    link.download = csvFileName;
+    link.dispatchEvent(new MouseEvent('click'));
+  }
+}
